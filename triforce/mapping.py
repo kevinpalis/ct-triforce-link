e5ee9f1d7b0862c3d3603e7f8001c613c3743b1a
Kevin Palis <kdp44@cornell.edu>
2022-04-18 17:26:06 -0400

    Loading all datafiles to dataframes.

--- triforce/mapping.py
+++ triforce/mapping.py
@@ -16,3 +16,14 @@ import traceback
 #just a test
 s = pd.Series([1, 3, 5, 7, 6, 8])
 print (s)
+
+entso = pd.read_csv('../data/entso.csv')
+platts = pd.read_csv('../data/platts.csv')
+gppd = pd.read_csv('../data/gppd.csv')
+
+print(entso.head())
+print(f"\nEntso RowCount: {len(entso.index)}")
+print(platts.head())
+print(f"\nPlatts RowCount: {len(platts.index)}")
+print(gppd.head())
+print(f"\nGPPD RowCount: {len(gppd.index)}")
