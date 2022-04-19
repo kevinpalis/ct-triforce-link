f5016682cca2ff0eecd05437fd15f987bfb3f736
Kevin Palis <kdp44@cornell.edu>
2022-04-19 13:53:42 -0400

    Finally clarified that there should be a max of 50 rows in the final output and only pick the best match. Changing code direction to that.

--- triforce/mapping.py
+++ triforce/mapping.py
@@ -21,16 +21,16 @@ entso = pd.read_csv('../data/entso.csv')
 platts = pd.read_csv('../data/platts.csv')
 gppd = pd.read_csv('../data/gppd.csv')
 
-print(entso.head())
+#print(entso.head())
 print(f"\nEntso RowCount: {len(entso.index)}")
-print(platts.head())
+#print(platts.head())
 print(f"\nPlatts RowCount: {len(platts.index)}")
-print(gppd.head())
+#print(gppd.head())
 print(f"\nGPPD RowCount: {len(gppd.index)}")
 
 
-entsoMin = entso[["entso_unit_id", "plant_name", "country", "unit_fuel"]]
-print(type(entsoMin))
+#entsoMin = entso[["entso_unit_id", "plant_name", "country", "unit_fuel"]]
+#print(type(entsoMin))
 
 #check how many platts_plant_id actually match
 q = "select p.platts_unit_id, p.plant_name as pName, g.plant_name as gName from platts p \
@@ -45,22 +45,26 @@ print(len(r.index))
 # print("Matched platts and gppd:")
 # print(len(gppd_platts.index))
 
-q2 = "select e.*, g.gppd_plant_id from entso e left join gppd g on (e.plant_name=g.plant_name \
-    and (e.country like '%' || g.country_long || '%' or g.country_long like '%' || e.country || '%') \
+q2 = "select e.*, g.gppd_plant_id, g.plant_name from entso e left join gppd g on ((e.plant_name like '%' || g.plant_name || '%' or \
+     g.plant_name like '%' || e.plant_name || '%') and (e.country like '%' || g.country_long || '%' or g.country_long like '%' || e.country || '%') \
         and e.unit_fuel=g.plant_primary_fuel)"
 entso_gppd = pysqldf(q2)
 print("Matched platts and gppd:")
 print(len(entso_gppd.index))
 print(entso_gppd)
 
-q3 = "select eg.*, p.platts_unit_id from entso_gppd eg left join platts p on (eg.plant_name=p.plant_name \
-    and (eg.country like '%' || p.country || '%' or p.country like '%' || eg.country || '%') \
+q3 = "select eg.*, p.platts_unit_id, p.unit_fuel, p.country from entso_gppd eg left join platts p on ((eg.plant_name like '%' || p.plant_name || '%' or \
+     p.plant_name like '%' || eg.plant_name || '%') and (eg.country like '%' || p.country || '%' or p.country like '%' || eg.country || '%') \
         and eg.unit_fuel=p.unit_fuel)"
 entso_gppd_platts = pysqldf(q3)
 print("Matched entso_gppd_platts:")
 print(len(entso_gppd_platts.index))
 print(entso_gppd_platts)
 
+entso_gppd_platts.drop_duplicates(subset=['entso_unit_id'], inplace=True)
+print("Removed duplicates entso_gppd_platts:")
+print(len(entso_gppd_platts.index))
+print(entso_gppd_platts)
 # for i in range(len(entso.index)):
 #     p1 = entso["plant_name"].iloc[i]
 #     print(p1)
