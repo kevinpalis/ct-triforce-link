dfd3b5a0be4d29911a9d880ed3d7ff660b4344f2
Kevin Palis <kdp44@cornell.edu>
2022-04-18 23:57:14 -0400

    A bunch of trial approaches. Currently struggling to find an efficient way to match.

--- triforce/mapping.py
+++ triforce/mapping.py
@@ -7,15 +7,15 @@
 '''
 import pandas as pd
 from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 from datetime import datetime, date
+from pandasql import sqldf
 
 import sys
 import getopt
 import traceback
 
-#just a test
-s = pd.Series([1, 3, 5, 7, 6, 8])
-print (s)
+pysqldf = lambda q: sqldf(q, globals())
 
 entso = pd.read_csv('../data/entso.csv')
 platts = pd.read_csv('../data/platts.csv')
@@ -27,3 +27,29 @@ print(platts.head())
 print(f"\nPlatts RowCount: {len(platts.index)}")
 print(gppd.head())
 print(f"\nGPPD RowCount: {len(gppd.index)}")
+
+
+entsoMin = entso[["entso_unit_id", "plant_name", "country", "unit_fuel"]]
+print(type(entsoMin))
+
+#check how many platts_plant_id actually match
+q = "select p.platts_unit_id, p.plant_name as pName, g.plant_name as gName from platts p inner join gppd g on p.platts_plant_id=g.platts_plant_id"
+r = pysqldf(q)
+print("Matching platts_plant_id:")
+print(r)
+
+# for i in range(len(entso.index)):
+#     p1 = entso["plant_name"].iloc[i]
+#     print(p1)
+#     print(process.extractBests(p1, platts['plant_name'], score_cutoff=90))
+#     print('\n')
+
+#platts
+print(entso["plant_name"].apply(lambda x: process.extractBests(x, platts["plant_name"].to_list(),score_cutoff=90)))
+#gppd
+print(entso["plant_name"].apply(lambda x: process.extractBests(x, gppd["plant_name"].to_list(),score_cutoff=90)))
+
+#test query
+#q = "SELECT * FROM entso where unit_capacity < 400 LIMIT 10"
+#r = pysqldf(q)
+#print(r)
