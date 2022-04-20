f3090aac26683d75f7fdab46d4bf4327d4bde464
Kevin Palis <kdp44@cornell.edu>
2022-04-19 20:59:58 -0400

    Organized verbosity. Added actual writing to output file.

--- triforce/mapping.py
+++ triforce/mapping.py
@@ -46,37 +46,41 @@ def main(argv):
             if arg == "True":
                 normalize_plant_names = True
         elif opt in ("-v", "--verbose"):
-            isVerbose = True
+            is_verbose = True
 
     entso = pd.read_csv(e_file)
     platts = pd.read_csv(p_file)
     gppd = pd.read_csv(g_file)
     
-    #print(entso.head())
-    print(f"\nEntso RowCount: {len(entso.index)}")
-    #print(platts.head())
-    print(f"\nPlatts RowCount: {len(platts.index)}")
-    #print(gppd.head())
-    print(f"\nGPPD RowCount: {len(gppd.index)}")
+    #A quick overview of row counts if ran verbose
+    if is_verbose:
+        #print(entso.head())
+        print(f"Datafiles Loaded. Counts: \nEntso Rows = {len(entso.index)}")
+        #print(platts.head())
+        print(f"Platts Rows = {len(platts.index)}")
+        #print(gppd.head())
+        print(f"GPPD Rows = {len(gppd.index)}\n")
 
+    #Attempt to cleanup/normalize plant_names for both platts and gppd using the plant_names from entso as primary reference, if normalize_plant_names is set to True.
+    #This uses fuzzy matching with a score cutoff of 90. By default, extract functions use the scorer WRatio (ie. Weighted Ratio)
+    #  which I think is best used in this case as I'm not very familiar with all the caveats fo matching power plant names across these 3 data sources.
+    #  WRatio gives us a good scorer for the widest range of fuzzy matches.
 
-    #Attempt to cleanup plant_names for both platts and gppd using the plant_names from entso as primary reference
-    #This uses fuzzy matching with a score cutoff of 90.
-    #get a list of reference plant_name from entso
-    entso_plant_names = entso['plant_name'].tolist()
-    print(entso_plant_names)
-
-    #for each reference plant_name
     if normalize_plant_names == True:
-        print("Cleaning up platts plant names...")
+        #get a list of reference plant_name from entso
+        entso_plant_names = entso['plant_name'].tolist()
+        #print(entso_plant_names)
+        #Normalize Platts plant names
+        print("Normalizing platts plant names...")
+        #for each reference plant_name
         for plant_name in entso_plant_names:
             # find matches that pass the score requirement
             matches = process.extractBests(plant_name, platts['plant_name'], score_cutoff=90)
             #for each match, replace the platss.plant_name with the reference plant_name from entso
             for match in matches:
                 platts.loc[platts['plant_name'] == match[0], 'plant_name'] = plant_name
-
-        print("Cleaning up gppd plant names...")
+        #do the same for GPPD
+        print("Normalizing gppd plant names...")
         for plant_name in entso_plant_names:
             # find matches that pass the score requirement
             matches = process.extractBests(plant_name, gppd['plant_name'], score_cutoff=90)
@@ -85,130 +89,74 @@ def main(argv):
                 gppd.loc[gppd['plant_name'] == match[0], 'plant_name'] = plant_name
 
 
-    #entsoMin = entso[["entso_unit_id", "plant_name", "country", "unit_fuel"]]
-    #print(type(entsoMin))
-
-    #check how many platts_plant_id actually match
+    #Create a table/dataframe with matching platts_plant_id from platts and gppd
+    #We use this to further map plants in entso that our first round of queries cannot map.
+    #This is a secondary mapping strategy because the instruction mentioned that while platts_plant_id should map, it is sometimes incorrect or missing.
     q1 = "select p.*, g.gppd_plant_id from platts p \
         inner join gppd g on p.platts_plant_id=g.platts_plant_id"
     matched_platts_plant_id = sqldf(q1, locals())
-    print("Matching platts_plant_id:")
-    print(matched_platts_plant_id)
-    print(len(matched_platts_plant_id.index))
 
-    # q1 = "select g.* from gppd g left join platts p on (g.platts_plant_id=p.platts_plant_id \
-    #     or (g.plant_name=p.plant_name and g.country_long=p.country and g.plant_primary_fuel=p.unit_fuel))"
-    # gppd_platts = pysqldf(q1)
-    # print("Matched platts and gppd:")
-    # print(len(gppd_platts.index))
-
-    #Phase 1: Match using the following criteria:
-    #For all rows in ENTSO, match if following conditions are met: plant_name is the same or similar to (ie. a substring of) GPPD plant AND the country is the same or similar, 
-    #and the unit_fuel is the same.
+    print("Starting Phase 1 mapping...")
+    #Phase 1: Map using the following criteria (most reliable):
+    #  For all rows in ENTSO, match if following conditions are met: plant_name is the same or similar to GPPD plant_name AND the country is the same or similar, AND the unit_fuel is the same.
+    #  Note that if normalize_plant_names was set, the name comparison would effectively include fuzzy matching, otherwise it is a less complicated substring match.
     q2 = "select e.*, g.gppd_plant_id from entso e left join gppd g on ((e.plant_name like '%' || g.plant_name || '%' or \
         g.plant_name like '%' || e.plant_name || '%') and (e.country like '%' || g.country_long || '%' or g.country_long like '%' || e.country || '%') \
             and e.unit_fuel=g.plant_primary_fuel)"
     entso_gppd = sqldf(q2, locals())
-    print("Matched platts and gppd:")
-    print(len(entso_gppd.index))
-    print(entso_gppd)
 
-    #For all rows in ENTSO, match if following conditions are met: plant_name is the same or similar to (ie. a substring of) Platts plant AND the country is the same or similar, 
-    #and the unit_fuel is the same.
+    #For all rows in ENTSO, match if following conditions are met: plant_name is the same or similar to Platts plant_name AND the country is the same or similar AND the unit_fuel is the same.
+    #  Note that if normalize_plant_names was set, the name comparison would effectively include fuzzy matching, otherwise it is a less complicated substring match.
     q3 = "select eg.*, p.platts_unit_id from entso_gppd eg left join platts p on ((eg.plant_name like '%' || p.plant_name || '%' or \
         p.plant_name like '%' || eg.plant_name || '%') and (eg.country like '%' || p.country || '%' or p.country like '%' || eg.country || '%') \
             and eg.unit_fuel=p.unit_fuel)"
     entso_gppd_platts = sqldf(q3, locals())
-    print("Matched entso_gppd_platts:")
-    print(len(entso_gppd_platts.index))
-    print(entso_gppd_platts)
 
+    if is_verbose:
+        print ("Removing duplicates in the mapped tables/dataframes...")
     #Remove duplicates in place, keeping only the first occurence (most of the duplicates are introduced by mapping to Platts, ie. 1 GPPD plant maps to multiple Platts units)
     entso_gppd_platts.drop_duplicates(subset=['entso_unit_id'], inplace=True)
-    print("Removed duplicates entso_gppd_platts:")
-    print(len(entso_gppd_platts.index))
-    #print(entso_gppd_platts)
     #reset index as there are gaps after removing duplicate rows
     entso_gppd_platts=entso_gppd_platts.reset_index(drop=True)
-    print("Index reset entso_gppd_platts:")
-    print(entso_gppd_platts)
-    #print(entso_gppd_platts[entso_gppd_platts['platts_unit_id'].isnull()])
-    #print(entso_gppd_platts[entso_gppd_platts['gppd_plant_id'].isnull()])
 
-
-
+    #Phase 2: Map using the joined table/dataframe matched_platts_plant_id. As mentioned, this is less reliable so we only use this to map the ones we weren't able to map in Phase 1
+    #   This is also not as straightforward as Phase 1. For this phase we do the following:
+    #   1. Create a table/dataframe with the output of phase 1 (entso_gppd_platts) left joined to the matched_platts_plant_id table (intersection table of platts and gppd where platts_plant_id matches)
+    #       on platts_unit_id. This effectively gives us all gppd_plant_id from matched_platts_plant_id table.
+    #   2. Create a table/dataframe with the output of phase 1 (entso_gppd_platts) left joined to the matched_platts_plant_id table (intersection table of platts and gppd where platts_plant_id matches)
+    #       on gppd_plant_id. This effectively gives us all platts_unit_id from matched_platts_plant_id table.
+    #   3. Use the two dataframes/tables to replace null values (plants we weren't able to map in phase 1) on columns gppd_plant_id and platts_unit_id respectively.
+    print("Starting Phase 2 mapping...")
     q4 = "select egp.entso_unit_id, egp.unit_capacity, egp.unit_fuel, egp.country, egp.unit_name, egp.plant_name, egp.plant_capacity, mpp.gppd_plant_id, \
         egp.platts_unit_id from entso_gppd_platts egp left join matched_platts_plant_id mpp on egp.platts_unit_id=mpp.platts_unit_id"
     g1 = sqldf(q4, locals())
-    print("Joined to matched_platts_plant_id on platts_unit_id:")
-    print(len(g1.index))
-    print(g1)
 
     #Remove duplicates in place, keeping only the first occurence (most of the duplicates are introduced by mapping to Platts, ie. 1 GPPD plant maps to multiple Platts units)
     g1.drop_duplicates(subset=['entso_unit_id'], inplace=True)
-    print("Removed duplicates g1:")
-    print(len(g1.index))
     #reset index as there are gaps after removing duplicate rows
     g1=g1.reset_index(drop=True)
-    print(g1)
-
-    print("Before Fill:")
-    print(entso_gppd_platts)
-
-    print("Filled nulls from values of g1:")
+    if is_verbose:
+        print("Filling nulls in column gppd_plant_id with mappings from phase 2...")
     entso_gppd_platts.fillna(g1,inplace=True)
-    print(entso_gppd_platts)
 
     q5 = "select egp.entso_unit_id, egp.unit_capacity, egp.unit_fuel, egp.country, egp.unit_name, egp.plant_name, egp.plant_capacity, egp.gppd_plant_id, \
         mpp.platts_unit_id from entso_gppd_platts egp left join matched_platts_plant_id mpp on egp.gppd_plant_id=mpp.gppd_plant_id"
     g2 = sqldf(q5, locals())
-    print("Joined to matched_platts_plant_id on gppd_plant_id:")
-    print(len(g2.index))
-    #print(g2)
-
+    #remove duplicates
     g2.drop_duplicates(subset=['entso_unit_id'], inplace=True)
-    print("Removed duplicates g2:")
-    print(len(g2.index))
     #reset index as there are gaps after removing duplicate rows
     g2=g2.reset_index(drop=True)
-    print(g2)
 
-    print("Before Fill:")
-    print(entso_gppd_platts.platts_unit_id.isnull().sum())
-
-    print("Filled nulls from values of g1:")
+    if is_verbose:
+        print("Filling nulls in column platts_unit_id with mappings from phase 2...")
     entso_gppd_platts.fillna(g2,inplace=True)
-    print(entso_gppd_platts)
-    print(f"Null counts: platts_unit_id={entso_gppd_platts.platts_unit_id.isnull().sum()} , gppd_plant_id={entso_gppd_platts.gppd_plant_id.isnull().sum()}")
 
+    if is_verbose:
+        print(f"Mapper was able to map all ENTSO plants except for {entso_gppd_platts.platts_unit_id.isnull().sum()} platts_unit_id and {entso_gppd_platts.gppd_plant_id.isnull().sum()} gppd_plant_id")
 
-    # for i in range(len(entso_gppd_platts.index)):
-    #     if entso_gppd_platts.iloc[[i]].gppd_plant_id.isnull():
-    #         entso_gppd_platts.iloc[[i]].gppd_plant_id='HAIII'
-    #     print(entso_gppd_platts.iloc[i].gppd_plant_id)
-
-    #entso_gppd_platts[entso_gppd_platts['gppd_plant_id'].isnull(), ['gppd_plant_id']] = gppd.loc[gppd['platts_plant_id'].isin(platts['platts_plant_id']),'gppd_plant_id']
-    # entso_gppd_platts.loc[entso_gppd_platts.platts_unit_id.isnull(), 'platts_unit_id']='DAMNU!'
-    # print("Replaced NULLS entso_gppd_platts:")
-    # print(entso_gppd_platts)
-
-
-    # for i in range(len(entso.index)):
-    #     p1 = entso["plant_name"].iloc[i]
-    #     print(p1)
-    #     print(process.extractBests(p1, platts['plant_name'], score_cutoff=90))
-    #     print('\n')
-
-
-    #platts
-    #print(entso_gppd_platts["plant_name"].apply(lambda x: process.extractBests(x, platts["plant_name"].to_list(),score_cutoff=90)))
-    #gppd
-    #print(entso["plant_name"].apply(lambda x: process.extractBests(x, gppd["plant_name"].to_list(),score_cutoff=90)))
-
-    #test query
-    #q = "SELECT * FROM entso where unit_capacity < 400 LIMIT 10"
-    #r = pysqldf(q)
-    #print(r)
+    #write output
+    entso_gppd_platts.to_csv('mapping.csv', index=False, columns=['entso_unit_id', 'platts_unit_id', 'gppd_plant_id'])
+    print("Mapping finished. Output is written as mapping.csv in the execution directory.")
 
 #utility method for exception handling
 def exitWithException(eCode):
